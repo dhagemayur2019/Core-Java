1a6d17e7a6d7bd17694e8d1730eb24a908245b46
Mayur Dhage <67791146+dhagemayur2019@users.noreply.github.com>
2020-09-23 22:23:17 +0530

    Create 1_hello_mayur.java

---
+++ basic/1_hello_mayur.java
@@ -0,0 +1,11 @@
+package classwork;
+//1. Write a program to print 'Hello' on screen and then print your name on a separate line.
+public class Ass1_hello_mayur
+{
+	public static void main(String args[])
+	{
+		
+			System.out.println("Hello \n Mayur");
+		
+	}
+}
